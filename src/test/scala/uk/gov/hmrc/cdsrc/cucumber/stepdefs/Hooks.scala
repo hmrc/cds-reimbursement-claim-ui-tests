8a0feb76561c4502d0a6798f74191b5e283e3dfa
NarsiAgaram <25480192+NarsiAgaram@users.noreply.github.com>
2021-02-11 13:18:55 +0000

    CDSR-36:added method for screenshot

---
+++ src/test/scala/uk/gov/hmrc/cdsrc/cucumber/stepdefs/Hooks.scala
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ */
+
+package uk.gov.hmrc.cdsrc.cucumber.stepdefs
+
+import io.cucumber.scala.{EN, ScalaDsl, Scenario}
+import org.openqa.selenium.{OutputType, TakesScreenshot}
+import uk.gov.hmrc.cdsrc.driver.BrowserDriver
+
+class Hooks extends ScalaDsl with EN with BrowserDriver {
+  After {
+    scenario: Scenario =>
+      if (scenario.isFailed) {
+        val screenshotName = scenario.getName.replaceAll(" ", "_")
+        val screenshot = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.BYTES)
+        scenario.attach(screenshot, "image/png", screenshotName)
+      }
+  }
+}
+
