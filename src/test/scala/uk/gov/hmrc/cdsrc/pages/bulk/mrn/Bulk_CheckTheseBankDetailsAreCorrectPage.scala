91688114ce6ca9e1cb75e7d878e88418089f42cb
NarsiAgaram <25480192+NarsiAgaram@users.noreply.github.com>
2021-08-12 12:06:46 +0100

    CDSR-675: updated tests

--- src/test/scala/uk/gov/hmrc/cdsrc/pages/bulk/mrn/Bulk_CheckTheseBankDetailsAreCorrectPage.scala
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/bulk/mrn/Bulk_CheckTheseBankDetailsAreCorrectPage.scala
@@ -14,15 +14,15 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.cdsrc.pages.individual.mrn
+package uk.gov.hmrc.cdsrc.pages.bulk.mrn
 
 import org.openqa.selenium.By
 import uk.gov.hmrc.cdsrc.conf.TestConfiguration
 import uk.gov.hmrc.cdsrc.pages.BasePage
 
-object Mrn_CheckTheseBankDetailsAreCorrectPage extends BasePage {
+object Bulk_CheckTheseBankDetailsAreCorrectPage extends BasePage {
 
-  override val url: String = TestConfiguration.url("cds-frontend") + "/single/check-these-bank-details-are-correct"
+  override val url: String = TestConfiguration.url("cds-frontend") + "/bulk/check-these-bank-details-are-correct"
   override val title = "Check these bank details are correct"
 
   override def expectedPageErrorTitle: Option[String] = Some("Check these bank details are correct - Claim for reimbursement of import duties - GOV.UK")
