71f96d466e623dcf9b71fb7ff7245d38b9cccd15
Jayen Kerai <jayen.kerai@digital.hmrc.gov.uk>
2021-09-06 16:52:37 +0100

    update bulk check answers page

--- src/test/scala/uk/gov/hmrc/cdsrc/pages/bulk/mrn/Bulk_CheckAnswersAcceptSendPage.scala
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/bulk/mrn/Bulk_CheckAnswersAcceptSendPage.scala
@@ -78,9 +78,9 @@ object Bulk_CheckAnswersAcceptSendPage extends BasePage {
         driver.findElement(By.cssSelector("#main-content > div > div > dl:nth-child(11) > div > dd.govuk-summary-list__actions > a")).getText should equal("Change\nReason")
 
         driver.findElement(By.cssSelector("#main-content > div > div > h2:nth-child(12)")).getText should equal("Commodity details")
-        driver.findElement(By.cssSelector("#main-content > div > div > dl:nth-child(13) > div > dt")).getText should equal("Commodities details")
+        driver.findElement(By.cssSelector("#main-content > div > div > dl:nth-child(13) > div > dt")).getText should equal("Details of the commodities")
         driver.findElement(By.cssSelector("#main-content > div > div > dl:nth-child(13) > div > dd.govuk-summary-list__value")).getText should equal("under 500 characters")
-        driver.findElement(By.cssSelector("#main-content > div > div > dl:nth-child(13) > div > dd.govuk-summary-list__actions > a")).getText should equal("Change\nCommodities details")
+        driver.findElement(By.cssSelector("#main-content > div > div > dl:nth-child(13) > div > dd.govuk-summary-list__actions > a")).getText should equal("Change\nDetails of the commodities")
 
         driver.findElement(By.cssSelector("#main-content > div > div > h2:nth-child(14)")).getText should equal("Claim calculation")
         driver.findElement(By.cssSelector("#main-content > div > div > dl:nth-child(15) > div:nth-child(1) > dt")).getText should equal("All UK duties to reimburse claimed by you")
