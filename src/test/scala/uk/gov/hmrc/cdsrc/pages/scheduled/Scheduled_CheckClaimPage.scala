5ce686ec01750b743c2726eeab5a5ad529bbfd90
Narasimha Agaram <25480192+NarsiAgaram@users.noreply.github.com>
2021-11-18 14:18:54 +0000

    updated h1 text (#146)

--- src/test/scala/uk/gov/hmrc/cdsrc/pages/scheduled/Scheduled_CheckClaimPage.scala
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/scheduled/Scheduled_CheckClaimPage.scala
@@ -22,12 +22,12 @@ import uk.gov.hmrc.cdsrc.pages.BasePage
 object Scheduled_CheckClaimPage extends BasePage {
 
   override val url: String = TestConfiguration.url("cds-frontend") + "/scheduled/check-claim"
-  override val title = "Check the reimbursement claim totals for your MRN"
+  override val title = "Check the reimbursement claim totals for all MRNs"
 
-  override def expectedPageErrorTitle: Option[String] = Some("Check the reimbursement claim totals for your MRN - Claim for reimbursement of import duties - GOV.UK")
+  override def expectedPageErrorTitle: Option[String] = Some("Check the reimbursement claim totals for all MRNs - Claim for reimbursement of import duties - GOV.UK")
 
-  override def expectedPageTitle: Option[String] = Some("Check the reimbursement claim totals for your MRN - Claim for reimbursement of import duties - GOV.UK")
+  override def expectedPageTitle: Option[String] = Some("Check the reimbursement claim totals for all MRNs - Claim for reimbursement of import duties - GOV.UK")
 
-  override def expectedPageHeader: Option[String] = Some("Check the reimbursement claim totals for your MRN")
+  override def expectedPageHeader: Option[String] = Some("Check the reimbursement claim totals for all MRNs")
 
 }
