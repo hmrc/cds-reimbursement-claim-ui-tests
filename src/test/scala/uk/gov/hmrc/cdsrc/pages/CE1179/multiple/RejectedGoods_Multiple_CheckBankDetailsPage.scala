28a66f62874c17b7e7168e29645a01791cc56058
Narasimha Agaram <25480192+NarsiAgaram@users.noreply.github.com>
2022-02-28 12:29:34 +0000

    Added bank details pages for multiple (#188)

---
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/CE1179/multiple/RejectedGoods_Multiple_CheckBankDetailsPage.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsrc.pages.CE1179.multiple
+
+import uk.gov.hmrc.cdsrc.conf.TestConfiguration
+import uk.gov.hmrc.cdsrc.pages.BasePage
+
+object RejectedGoods_Multiple_CheckBankDetailsPage extends BasePage {
+
+  override val url: String = TestConfiguration.url("cds-frontend") + "/rejected-goods/multiple/check-bank-details"
+  override val title = "Check these bank details are correct"
+
+  override def expectedPageErrorTitle: Option[String] = Some("Check these bank details are correct - Claim for reimbursement of import duties - GOV.UK")
+
+  override def expectedPageTitle: Option[String] = Some("Check these bank details are correct - Claim for reimbursement of import duties - GOV.UK")
+
+  override def expectedPageHeader: Option[String] = Some("Check these bank details are correct")
+
+  override def clickContinueButton(): Unit = {
+    click on cssSelector("#main-content > div > div > a")
+  }
+
+}
