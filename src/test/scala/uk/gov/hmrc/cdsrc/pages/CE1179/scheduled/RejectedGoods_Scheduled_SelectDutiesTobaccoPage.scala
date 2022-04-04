5e104696eb608f82830f26d145741a7fed8f2054
Jayen Kerai <jayen.kerai@digital.hmrc.gov.uk>
2022-04-04 15:35:56 +0100

    CDSR-1379: added new pages

---
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/CE1179/scheduled/RejectedGoods_Scheduled_SelectDutiesTobaccoPage.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsrc.pages.CE1179.scheduled
+
+import uk.gov.hmrc.cdsrc.conf.TestConfiguration
+import uk.gov.hmrc.cdsrc.pages.BasePage
+
+object RejectedGoods_Scheduled_SelectDutiesTobaccoPage extends BasePage {
+
+  override val url: String = TestConfiguration.url("cds-frontend") + "/rejected-goods/scheduled/select-duties/tobacco"
+  override val title = "Select the tobacco products duties you want to claim for all MRNs in the file you uploaded"
+
+  override def expectedPageErrorTitle: Option[String] = Some("Select the tobacco products duties you want to claim for all MRNs in the file you uploaded - Claim for reimbursement of import duties - GOV.UK")
+
+  override def expectedPageTitle: Option[String] = Some("Select the tobacco products duties you want to claim for all MRNs in the file you uploaded - Claim for reimbursement of import duties - GOV.UK")
+
+  override def expectedPageHeader: Option[String] = Some("Select the tobacco products duties you want to claim for all MRNs in the file you uploaded")
+
+}
