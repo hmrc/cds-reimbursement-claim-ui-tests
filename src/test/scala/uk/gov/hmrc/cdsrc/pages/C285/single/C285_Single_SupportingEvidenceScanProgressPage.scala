b622adbc082f7d8a726fcdfae5772e3a665414f0
Jayen Kerai <jayen.kerai@digital.hmrc.gov.uk>
2022-05-31 13:31:48 +0100

    CDSR-1530: changed unnamed to C285

---
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/C285/single/C285_Single_SupportingEvidenceScanProgressPage.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsrc.pages.C285.single
+
+import org.scalatest.Assertion
+import uk.gov.hmrc.cdsrc.conf.TestConfiguration
+import uk.gov.hmrc.cdsrc.pages.BasePage
+
+object C285_Single_SupportingEvidenceScanProgressPage extends BasePage {
+
+  override val url: String = TestConfiguration.url("cds-frontend") + "/single/supporting-evidence/scan-progress"
+  override val title = "Wait a few seconds and then select ‘continue’"
+
+  override def expectedPageErrorTitle: Option[String] = Some("Wait a few seconds and then select ‘continue’ - Claim back import duty and VAT - GOV.UK")
+
+  override def expectedPageTitle: Option[String] = Some("Wait a few seconds and then select ‘continue’ - Claim back import duty and VAT - GOV.UK")
+
+  override def expectedPageHeader: Option[String] = Some("Wait a few seconds and then select ‘continue’")
+
+  override def checkURL: Assertion = {
+    driver.getCurrentUrl should fullyMatch regex (url + ".*?").r
+  }
+
+}
