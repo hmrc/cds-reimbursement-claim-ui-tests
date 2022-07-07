f9d79c1512abe4d9ee1822e799ec32252f5a3c4e
NarsiAgaram <25480192+NarsiAgaram@users.noreply.github.com>
2022-07-07 14:10:34 +0100

    CDSR-1005:updated h1

--- src/test/scala/uk/gov/hmrc/cdsrc/pages/common/CheckEoriDetailsPage.scala
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/common/CheckEoriDetailsPage.scala
@@ -22,13 +22,13 @@ import uk.gov.hmrc.cdsrc.pages.BasePage
 object CheckEoriDetailsPage extends BasePage {
 
   override val url: String = TestConfiguration.url("cds-frontend") + "/check-eori-details"
-  override val title = "Check these EORI details are correct"
+  override val title = "Check the EORI associated with the Government Gateway Name is correct"
 
-  override def expectedPageErrorTitle: Option[String] = Some("Check these EORI details are correct - Claim back import duty and VAT - GOV.UK")
+  override def expectedPageErrorTitle: Option[String] = Some("Check the EORI associated with the Government Gateway Name is correct - Claim back import duty and VAT - GOV.UK")
 
-  override def expectedPageTitle: Option[String] = Some("Check these EORI details are correct - Claim back import duty and VAT - GOV.UK")
+  override def expectedPageTitle: Option[String] = Some("Check the EORI associated with the Government Gateway Name is correct - Claim back import duty and VAT - GOV.UK")
 
-  override def expectedPageHeader: Option[String] = Some("Check these EORI details are correct")
+  override def expectedPageHeader: Option[String] = Some("Check the EORI associated with the Government Gateway Name is correct")
 
   override def clickRadioButton(text: String) {
     text.toLowerCase() match {
