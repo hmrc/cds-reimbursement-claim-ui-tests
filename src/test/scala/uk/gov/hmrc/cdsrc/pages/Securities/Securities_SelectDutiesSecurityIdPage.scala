a4486a880128bd1915781861aa1bdaedc45eed21
Chiran San <56673779+chiransan01@users.noreply.github.com>
2022-07-25 16:43:36 +0100

    Securities - Select duties page added (#246)
    * Securities - Select duties page added
    
    * Locator updated
    
    * Locator updated

---
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/Securities/Securities_SelectDutiesSecurityIdPage.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsrc.pages.Securities
+
+import org.openqa.selenium.By
+import org.scalatest.Assertion
+import uk.gov.hmrc.cdsrc.conf.TestConfiguration
+import uk.gov.hmrc.cdsrc.pages.BasePage
+
+object Securities_SelectDutiesSecurityIdPage extends BasePage {
+
+  override val url: String = TestConfiguration.url("cds-frontend") + "/securities/select-duties/..."
+  override val title = "Select the duties you want to claim for"
+
+  override def expectedPageHeader: Option[String] = Some("Select the duties you want to claim for")
+
+  override def expectedPageErrorTitle: Option[String] = Some("ERROR: Select the duties you want to claim for - Claim back import duty and VAT - GOV.UK")
+
+  override def expectedPageTitle: Option[String] = Some("Select the duties you want to claim for - Claim back import duty and VAT - GOV.UK")
+
+  override def checkPageHeader(): Assertion =
+    true should equal(true)
+
+   def checkSecurityId(securityID: String): Unit =
+    driver.findElement(By cssSelector "#main-content > div > div > form > span").getText should equal(s"Security ID: $securityID")
+}
