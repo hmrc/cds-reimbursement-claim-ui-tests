e864b83fef2c7cd23a8da4cf0d601d47a8da08a4
Chiran San <56673779+chiransan01@users.noreply.github.com>
2022-07-29 14:23:29 +0100

    New page 'Securities_ConfirmFullRepaymentSecurityIdPage' added (#253)

---
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/Securities/Securities_ChooseBankAccountTypePage.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsrc.pages.Securities
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.cdsrc.conf.TestConfiguration
+import uk.gov.hmrc.cdsrc.pages.BasePage
+
+
+object Securities_ChooseBankAccountTypePage extends BasePage {
+
+  override val url: String = TestConfiguration.url("cds-frontend") + "/securities/choose-bank-account-type"
+  override val title = "What type of account details are you providing?"
+
+  override def expectedPageErrorTitle: Option[String] = Some(
+    "What type of account details are you providing? - Claim back import duty and VAT - GOV.UK"
+  )
+
+  override def expectedPageTitle: Option[String] = Some(
+    "What type of account details are you providing? - Claim back import duty and VAT - GOV.UK"
+  )
+
+  override def expectedPageHeader: Option[String] = Some("What type of account details are you providing?")
+  override def checkPageTitle(duty: String): Unit =
+    driver.findElement(By cssSelector ".govuk-fieldset__heading").getText should equal(title)
+
+  override def checkPageErrorTitle(duty: String): Unit =
+    driver.findElement(By cssSelector ".govuk-fieldset__heading").getText should equal(title)
+
+  override def clickRadioButton(text: String): Unit = {
+    text.toLowerCase() match {
+      case "business account" => click on cssSelector("#select-bank-account-type-business-bank-account")
+      case "personal account" => click on cssSelector("#select-bank-account-type-personal-bank-account")
+    }
+  }
+}
