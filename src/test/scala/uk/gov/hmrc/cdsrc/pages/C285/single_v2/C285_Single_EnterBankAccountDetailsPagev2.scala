ac6cae96c1301c0313a851f4004b9d29c328804d
madhubio <madhu.san81@gmail.com>
2023-02-28 22:40:07 +0000

    Overpayments v2 features added for C285 single journey

---
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/C285/single_v2/C285_Single_EnterBankAccountDetailsPagev2.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsrc.pages.C285.single_v2
+
+import uk.gov.hmrc.cdsrc.conf.TestConfiguration
+import uk.gov.hmrc.cdsrc.pages.BasePage
+
+object C285_Single_EnterBankAccountDetailsPagev2 extends BasePage {
+
+  override val url: String = TestConfiguration.url("cds-frontend") + "/overpayments/v2/single/enter-bank-account-details"
+  override val title = "Enter your bank account details"
+
+  override def expectedPageErrorTitle: Option[String] = Some("Enter your bank account details - Claim back import duty and VAT - GOV.UK")
+
+  override def expectedPageTitle: Option[String] = Some("Enter your bank account details - Claim back import duty and VAT - GOV.UK")
+
+  override def expectedPageHeader: Option[String] = Some("Enter your bank account details")
+
+  override def enterDetails(data: String): Unit = {
+    val bankAccountName: String = "Mr John Smith"
+    val sortCode: String = "123456"
+    val accountNumber: String = "11001001"
+
+    enterText("enter-bank-account-details.account-name", bankAccountName)
+    enterText("enter-bank-account-details.sort-code", sortCode)
+    enterText("enter-bank-account-details.account-number", accountNumber)
+  }
+
+  override def selectCheckBox(): Unit = {
+    click on cssSelector("#enter-bank-details_is-business-account")
+  }
+
+}
