9750b9a5034b9695b3a6d867f089fc988f86f8ca
lathamurugan11 <117362434+lathamurugan11@users.noreply.github.com>
2023-10-03 13:24:34 +0100

    Developed CDSR-2467 CE1179 Multiple Subsidy

--- src/test/scala/uk/gov/hmrc/cdsrc/pages/CE1179/multiple/CE1179_Multiple_SelectDuties1DutyPage.scala
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/CE1179/multiple/CE1179_Multiple_SelectDuties1DutyPage.scala
@@ -24,13 +24,13 @@ import uk.gov.hmrc.cdsrc.pages.BasePage
 object CE1179_Multiple_SelectDuties1DutyPage extends BasePage {
 
   override val url: String = TestConfiguration.url("cds-frontend") + "/rejected-goods/multiple/enter-claim/1/..."
-  override val title = ""
+  override val title = "Claim details for A80 - Definitive Anti-Dumping Duty under first MRN"
 
-  override def expectedPageErrorTitle: Option[String] = Some("")
+  override def expectedPageErrorTitle: Option[String] = Some("Claim details for A80 - Definitive Anti-Dumping Duty under first MRN - Claim back import duty and VAT - GOV.UK")
 
-  override def expectedPageTitle: Option[String] = Some("")
+  override def expectedPageTitle: Option[String] = Some("Claim details for A80 - Definitive Anti-Dumping Duty under first MRN - Claim back import duty and VAT - GOV.UK")
 
-  override def expectedPageHeader: Option[String] = Some("")
+  override def expectedPageHeader: Option[String] = Some("Claim details for A80 - Definitive Anti-Dumping Duty under first MRN")
 
   override def checkPageHeader(): Assertion = {
     true should equal(true)
@@ -42,11 +42,11 @@ object CE1179_Multiple_SelectDuties1DutyPage extends BasePage {
   }
 
   override def checkPageTitle(duty: String): Unit = {
-    driver.findElement(By cssSelector "#main-content > div > div > h1").getText should equal(s"Claim details for $duty under first MRN")
+    driver.findElement(By cssSelector "#main-content > div > div > h1").getText should equal(s"Claim details for $duty")
   }
 
   override def checkPageErrorTitle(duty: String): Unit = {
-    driver.findElement(By cssSelector "#main-content > div > div > h1").getText should equal(s"Claim details for $duty under first MRN")
+    driver.findElement(By cssSelector "#main-content > div > div > h1").getText should equal(s"Claim details for $duty")
   }
 
 }
