ea20a37b3d0e708fa8943835b3391d468bb8a7af
madhubio <madhu.san81@gmail.com>
2023-12-08 14:36:17 +0000

    Updated Ui tests for the choose bank type url

--- src/test/scala/uk/gov/hmrc/cdsrc/pages/C285/single_v2/C285_Single_BankAccountTypePagev2.scala
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/C285/single_v2/C285_Single_BankAccountTypePagev2.scala
@@ -21,7 +21,7 @@ import uk.gov.hmrc.cdsrc.pages.BasePage
 
 object C285_Single_BankAccountTypePagev2 extends BasePage {
 
-  override val url: String = TestConfiguration.url("cds-frontend") + "/overpayments/single/bank-account-type"
+  override val url: String = TestConfiguration.url("cds-frontend") + "/overpayments/single/choose-bank-account-type"
   override val title = "What type of account details are you providing?"
 
   override def expectedPageErrorTitle: Option[String] = Some("What type of account details are you providing? - Claim back import duty and VAT - GOV.UK")
