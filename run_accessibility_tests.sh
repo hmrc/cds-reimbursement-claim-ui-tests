89a274728cdda60c11b556fa87a37466ff0b7572
DeekshaSrivastava29 <117635316+DeekshaSrivastava29@users.noreply.github.com>
2024-03-20 11:44:29 +0000

    change runner file (#425)

--- run_accessibility_tests.sh
+++ run_accessibility_tests.sh
@@ -1,13 +1,6 @@
-#!/bin/bash
-set -e
-ENV=${1:-local}
-BROWSER=${2:-chrome}
-DRIVER=
+#!/bin/bash -e
 
-if [ "$BROWSER" = "chrome" ]; then
-    DRIVER="-Dwebdriver.chrome.driver=/usr/local/bin/chromedriver"
-elif [ "$BROWSER" = "firefox" ]; then
-    DRIVER="-Dwebdriver.gecko.driver=/usr/local/bin/geckodriver"
-fi
+BROWSER=$1
+ENVIRONMENT=$2
 
-sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER -Ddrivernotquit=true "testOnly uk.gov.hmrc.cdsrc.cucumber.runner.AccessibilityRunner"
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.ui.cucumber.runner.AccessibilityRunner" testReport
--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,6 @@
 #!/bin/bash -e
-ENV=${1:-local}
-BROWSER=${2:-chrome}
-DRIVER=
 
-if [ "$BROWSER" = "chrome" ]; then
-    DRIVER="-Dwebdriver.chrome.driver=/usr/local/bin/chromedriver"
-elif [ "$BROWSER" = "firefox" ]; then
-    DRIVER="-Dwebdriver.gecko.driver=/usr/local/bin/geckodriver"
-fi
+BROWSER=$1
+ENVIRONMENT=$2
 
-# Scalafmt checks have been separated from the test command to avoid OutOfMemoryError in Jenkins
-#sbt scalafmtCheckAll scalafmtSbtCheck
-
-sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER "testOnly uk.gov.hmrc.cdsrc.cucumber.runner.Runner"
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.ui.cucumber.runner.Runner" testReport
