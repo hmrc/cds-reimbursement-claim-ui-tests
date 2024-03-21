22688244df63d62303f1cf1b6d78e973b7e2dad8
DeekshaSrivastava29 <dikss92@gmail.com>
2024-03-21 19:54:57 +0000

    change sh files

--- run_tests_wip.sh
+++ run_tests_wip.sh
@@ -3,4 +3,4 @@
 BROWSER=$1
 ENVIRONMENT=$2
 
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.ui.cucumber.runner.WipRunner" testReport
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.cdsrc.cucumber.runner.WipRunner" testReport
