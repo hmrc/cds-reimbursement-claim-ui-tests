5da419412807998af67ba114f9264bcdfd51da03
DeekshaSrivastava29 <117635316+DeekshaSrivastava29@users.noreply.github.com>
2024-09-11 13:45:56 +0100

    change id for radio button (#446)

--- src/test/scala/uk/gov/hmrc/cdsrc/pages/C285/multiple_V2/C285_Multiple_CheckClaimPagev2.scala
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/C285/multiple_V2/C285_Multiple_CheckClaimPagev2.scala
@@ -32,8 +32,8 @@ object C285_Multiple_CheckClaimPagev2 extends BasePage {
 
   override def clickRadioButton(text: String): Unit = {
     text.toLowerCase() match {
-      case "yes" => click on xpath(s"""//*[@id="check-claim.multiple"]""")
-      case "no" => click on xpath(s"""//*[@id="check-claim.multiple-2"]""")
+      case "yes" => click on xpath(s"""//*[@id="check-claim"]""")
+      case "no" => click on xpath(s"""//*[@id="check-claim-2"]""")
     }
   }
 
