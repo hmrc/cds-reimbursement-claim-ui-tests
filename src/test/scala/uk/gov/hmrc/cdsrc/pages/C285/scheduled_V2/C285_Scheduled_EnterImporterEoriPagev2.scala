151a566dcb61350dff53643a7075e3700767c9d0
noushan1 <nishmithaovr@gmail.com>
2025-02-28 14:44:54 +0000

    Signed commit

--- src/test/scala/uk/gov/hmrc/cdsrc/pages/C285/scheduled_V2/C285_Scheduled_EnterImporterEoriPagev2.scala
+++ src/test/scala/uk/gov/hmrc/cdsrc/pages/C285/scheduled_V2/C285_Scheduled_EnterImporterEoriPagev2.scala
@@ -30,7 +30,7 @@ object C285_Scheduled_EnterImporterEoriPagev2 extends BasePage {
 
   override def expectedPageHeader: Option[String] = Some("What's the importer's EORI number?")
 
-  override def enterDetails(textToEnter: String) {
+  override def enterDetails(textToEnter: String): Unit = {
     enterText("enter-the-importer-eori-number", textToEnter)
   }
 
