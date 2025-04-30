4c7dd89e387fd9f978c91b2e8ff00b4ef97d067c
noushan1 <nishmithaovr@gmail.com>
2025-04-30 14:40:34 +0100

    Remove Importer and Declarant details on the CYA page V1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     "org.scalatestplus"      %% "selenium-4-12"   % "3.2.17.0" % Test,
     "com.vladsch.flexmark"    % "flexmark-all"    % "0.64.8"   % Test,
     "io.cucumber"            %% "cucumber-scala"  % "8.27.0"   % Test,
-    "io.cucumber"             % "cucumber-junit"  % "7.22.0"   % Test,
+    "io.cucumber"             % "cucumber-junit"  % "7.22.1"   % Test,
     "junit"                   % "junit"           % "4.13.2"   % Test,
     "com.novocode"            % "junit-interface" % "0.11"     % Test,
     "com.typesafe"            % "config"          % "1.4.3"    % Test,
