687865084fef30e080b51b4e4f6efee5422cac4b
AdamCoops <adam.cooper2@digital.hmrc.gov.uk>
2026-01-19 12:13:40 +0000

    DCWL-4251-update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,9 +11,9 @@ object Dependencies {
     "io.cucumber"             % "cucumber-junit"  % "7.22.2"   % Test,
     "junit"                   % "junit"           % "4.13.2"   % Test,
     "com.novocode"            % "junit-interface" % "0.11"     % Test,
-    "com.typesafe"            % "config"          % "1.4.3"    % Test,
-    "ch.qos.logback"          % "logback-classic" % "1.5.18"   % Test,
-    "commons-io"              % "commons-io"      % "2.19.0"
+    "com.typesafe"            % "config"          % "1.4.5"    % Test,
+    "ch.qos.logback"          % "logback-classic" % "1.5.25"   % Test,
+    "commons-io"              % "commons-io"      % "2.21.0"
   )
 
 }
